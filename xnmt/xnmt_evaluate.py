5005e3dd7077a00a32debcc9682b02590d83cd73
DevSinghSachan <devendras@enixta.com>
2017-04-09 01:57:42 +0530

    xnmt_evaluate function code

---
+++ xnmt/xnmt_evaluate.py
@@ -0,0 +1,45 @@
+import argparse
+from evaluator import BLEUEvaluator
+
+
+def read_data(loc_):
+    """Reads the lines in the file specified in loc_ and return the list after inserting the tokens
+    """
+    data = list()
+    with open(loc_) as fp:
+        for line in fp:
+            t = line.split()
+            data.append(t)
+    return data
+    
+
+def xnmt_evaluate(args):
+    """"Returns the BLEU score of the hyp sentences using reference target sentences
+    """
+    
+    ref_corpus = read_data(args.ref_target_file_path)
+    hyp_corpus = read_data(args.hyp_target_file_path)
+
+    model = BLEUEvaluator(ngram=4)
+    bleu_score = model.evaluate(ref_corpus, hyp_corpus)
+    
+    return bleu_score
+        
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser("This script performs evaluation using BLEU score metric \
+                                     between the reference and candidate (hypothesis) target files ")
+
+    parser.add_argument('--ref_target_file_path',
+                        type=str,
+                        help='path of the reference file')
+
+    parser.add_argument('--hyp_target_file_path',
+                        type=str,
+                        help='path of the hypothesis target file')
+    
+    args = parser.parse_args()
+    
+    bleu_score = xnmt_evaluate(args)
+    print("BLEU Score = {}".format(bleu_score))
