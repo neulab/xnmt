c267f4695eb88521f00b8a5b3bccc4dd0d110616
Matthieu Felix <matthieufelix@gmail.com>
2017-04-24 10:45:51 -0400

    Fixed dimensionality error in decoder

--- xnmt/decoder.py
+++ xnmt/decoder.py
@@ -36,6 +36,7 @@ class MlpSoftmaxDecoder(Decoder):
 
   def get_scores(self, context):
     mlp_input = dy.concatenate([context, self.state.output()])
+    mlp_input = dy.reshape(mlp_input, (mlp_input.dim()[0][0],))
     scores = self.mlp(mlp_input)
     return scores
 
