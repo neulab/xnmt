010bbfe8149c404793f44591ed68ea1e015bdaa5
Matthias Sperber <matthias.sperber@gmail.com>
2017-06-22 14:01:26 +0200

    support partial length normalization during the beam search

--- xnmt/search_strategy.py
+++ xnmt/search_strategy.py
@@ -1,6 +1,7 @@
 import dynet as dy
 import numpy as np
 from length_normalization import *
+from vocab import Vocab
 
 class SearchStrategy:
   '''
@@ -17,12 +18,14 @@ class BeamSearch(SearchStrategy):
     # The only reason why we don't set NoNormalization as the default is because it currently
     # breaks our documentation pipeline
     self.len_norm = len_norm if len_norm != None else NoNormalization()
+    
+    self.entrs = []
 
   class Hypothesis:
-    def __init__(self, score, id, state):
+    def __init__(self, score, id_list, state):
       self.score = score
       self.state = state
-      self.id_list = id
+      self.id_list = id_list
 
 
 
@@ -37,7 +40,7 @@ class BeamSearch(SearchStrategy):
       new_set = []
       for hyp in active_hyp:
 
-        if hyp.id_list[-1] == 1:
+        if hyp.id_list[-1] == Vocab.ES:
           completed_hyp.append(hyp)
           continue
 
@@ -48,17 +51,19 @@ class BeamSearch(SearchStrategy):
         score = dy.log_softmax(decoder.get_scores(context)).npvalue()
         top_ids = np.argpartition(score, max(-len(score),-self.b))[-self.b:]
 
-        for id in top_ids:
+        for cur_id in top_ids:
           new_list = list(hyp.id_list)
-          new_list.append(id)
-          new_set.append(self.Hypothesis(hyp.score + score[id], new_list, decoder.state))
+          new_list.append(cur_id)
+          new_set.append(self.Hypothesis(self.len_norm.normalize_partial(hyp.score, score[cur_id], len(new_list)), 
+                                         new_list, 
+                                         decoder.state))
 
       active_hyp = sorted(new_set, key=lambda x: x.score, reverse=True)[:self.b]
 
     if len(completed_hyp) == 0:
       completed_hyp = active_hyp
 
-    self.len_norm.normalize_length(completed_hyp, src_length)
+    self.len_norm.normalize_completed(completed_hyp, src_length)
 
     result = sorted(completed_hyp, key=lambda x: x.score, reverse=True)[0]
     return result.id_list
