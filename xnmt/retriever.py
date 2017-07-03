85a8e9cd64e00d0b1f82cf599eb8a6cbe2fc713d
Graham Neubig <neubig@gmail.com>
2017-07-03 12:58:42 -0400

    Modularized translator

---
+++ xnmt/retriever.py
@@ -0,0 +1,83 @@
+from __future__ import division, generators
+
+import dynet as dy
+from batcher import *
+from search_strategy import *
+from vocab import Vocab
+from serializer import Serializable, DependentInitParam
+from train_test_interface import TrainTestInterface
+
+class Retriever(TrainTestInterface):
+  '''
+  A template class implementing a retrieval model.
+  '''
+
+  def calc_loss(self, db_idx):
+    '''Calculate loss based on a database index.
+
+    :param db_idx: The index in the database of the training example.
+    :returns: An expression representing the loss.
+    '''
+    raise NotImplementedError('calc_loss must be implemented for Retriever subclasses')
+
+  def index_database(self):
+    '''A function that can be called before actually performing retrieval.
+
+    This will perform any necessary pre-processing to make retrieval more efficient.
+    If the model is updated, assume that the indexing result is stale and no longer applicable.
+    '''
+    pass
+    
+
+  def retrieve(self, src):
+    '''Perform retrieval, trying to get the sentence that most closely matches in the database.
+
+    :param src: The source.
+    :returns: The ID of the example that most closely matches in the database.
+    '''
+    raise NotImplementedError('retrieve must be implemented for Retriever subclasses')
+
+  def set_train(self, val):
+    for component in self.get_train_test_components():
+      Retriever.set_train_recursive(component, val)
+  @staticmethod
+  def set_train_recursive(component, val):
+    component.set_train(val)
+    for sub_component in component.get_train_test_components():
+      Retriever.set_train_recursive(sub_component, val)
+
+
+class DotProductRetriever(Retriever, Serializable):
+  '''
+  A retriever trains using max-margin methods.
+  '''
+  
+  yaml_tag = u'!DotProductRetriever'
+
+
+  def __init__(self, src_embedder, src_encoder, trg_embedder, trg_encoder, database):
+    '''Constructor.
+
+    :param src_embedder: A word embedder for the source language
+    :param src_encoder: An encoder for the source language
+    :param trg_embedder: A word embedder for the target language
+    :param trg_encoder: An encoder for the target language
+    '''
+    self.src_embedder = src_embedder
+    self.src_encoder = src_encoder
+    self.trg_embedder = trg_embedder
+    self.trg_encoder = trg_encoder
+    self.database = database
+  
+  def get_train_test_components(self):
+    return [self.src_encoder, self.trg_encoder]
+
+  def calc_loss(self, db_idx):
+    raise NotImplementedError("calc_loss needs to calculate the max-margin objective")
+
+  def index_database(self):
+    raise NotImplementedError("index_database needs to calculate the vectors for all the elements in the database and find the closest")
+
+  def retrieve(self, src):
+    raise NotImplementedError("retrieve needs find the example index with the largest dot product")
+
