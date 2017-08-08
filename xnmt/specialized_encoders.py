e6949451c567734d3243d4b423f82f65c4ad9974
Mingxing DU <fdsyen@gmail.com>
2017-08-07 20:24:48 -0400

    report the error I got

--- xnmt/specialized_encoders.py
+++ xnmt/specialized_encoders.py
@@ -8,6 +8,88 @@ from encoder import *
 
 # This is a CNN-based encoder that was used in the following paper:
 #  http://papers.nips.cc/paper/6186-unsupervised-learning-of-spoken-language-with-visual-context.pdf
+class TilburgSpeechEncoder(Encoder, Serializable):
+  yaml_tag = u'!TilburgSpeechEncoder'
+  def __init__(self, filter_height, filter_width, channels, num_filters, stride, rhn_num_hidden_layers, rhn_dim, rhn_microsteps, attention_dim, residual= False):
+    """
+    :param etc.
+    """
+    self.filter_height = filter_height
+    self.filter_width = filter_width
+    self.channels = channels
+    self.num_filters = num_filters
+    self.stride = stride
+    self.rhn_num_hidden_layers = rhn_num_hidden_layers
+    self.rhn_dim = rhn_dim
+    self.rhn_microsteps = rhn_microsteps
+    self.attention_dim = attention_dim
+    self.residual = residual
+
+    model = model_globals.dynet_param_collection.param_col
+    # Convolutional layer
+    self.filter_conv = model.add_parameters(dim=(self.filter_height, self.filter_width, self.channels, self.num_filters))
+    # Recurrent highway layer
+    self.recur  = []
+    self.linear = []
+    self.init   = []
+    self.attention = []
+
+    input_dim = num_filters
+    for l in range(rhn_num_hidden_layers):
+      self.init.append(model.add_parameters((rhn_dim,)))
+      self.linear.append((model.add_parameters((rhn_dim, input_dim)),
+                          model.add_parameters((rhn_dim, input_dim,))))
+      input_dim = rhn_dim
+      recur_layer = []
+      for m in range(self.rhn_microsteps):
+        recur_layer.append((model.add_parameters((rhn_dim, rhn_dim)),
+                            model.add_parameters((rhn_dim,)),
+                            model.add_parameters((rhn_dim, rhn_dim,)),
+                            model.add_parameters((rhn_dim,))))
+      self.recur.append(recur_layer)
+    # Attention layer
+    self.attention.append((model.add_parameters((attention_dim, rhn_dim)),
+                           model.add_parameters(attention_dim, )))
+
+  def transduce(self, src):
+    src = src.as_tensor()
+    # convolutional layer
+    l1 = dy.rectify(dy.conv2d(src, dy.parameter(self.filter_conv), stride = [self.stride, self.stride], is_valid = True))
+    timestep = l1.dim()[0][1]
+    features = l1.dim()[0][2]
+    batch_size = l1.dim()[1]
+    # transpose l1 to be (timesetp, dim), but keep the batch_size.
+    rhn_in = dy.reshape(l1, (timestep, features), batch_size = batch_size)
+    rhn_in = [dy.pick(rhn_in, i) for i in range(timestep)]
+    for l in range(self.rhn_num_hidden_layers):
+      rhn_out = []
+      # initialize a random vector for the first state vector, keep the same batch size.
+      prev_state = dy.parameter(self.init[l])
+      # begin recurrent high way network
+      for t in range(timestep):
+        for m in range(0, self.rhn_microsteps):
+          H = dy.affine_transform([dy.parameter(self.recur[l][m][1]), dy.parameter(self.recur[l][m][0]),  prev_state])
+          T = dy.affine_transform([dy.parameter(self.recur[l][m][3]), dy.parameter(self.recur[l][m][2]),  prev_state])
+          if m == 0:
+            H += dy.parameter(self.linear[l][0]) * rhn_in[t]
+            T += dy.parameter(self.linear[l][1]) * rhn_in[t]
+          H = dy.tanh(H)
+          T = dy.logistic(T)
+          prev_state = dy.cmult(1 - T, prev_state) + dy.cmult(T, H) # ((1024, ), batch_size)
+        rhn_out.append(prev_state)
+      if self.residual and l>0:
+        rhn_out = [sum(x) for x in zip(rhn_out, rhn_in)]
+      rhn_in = rhn_out
+    # Compute the attention-weighted average of the activations
+    rhn_in = dy.concatenate_cols(rhn_in)
+    scores = dy.transpose(dy.parameter(self.attention[0][1]))*dy.tanh(dy.parameter(self.attention[0][0])*rhn_in) # ((1,510), batch_size)
+    scores = dy.reshape(scores, (scores.dim()[0][1],), batch_size = scores.dim()[1])
+    attn_out = rhn_in*dy.softmax(scores) # # rhn_in.as_tensor() is ((1024,510), batch_size) softmax is ((510,), batch_size)
+    return expression_sequence.ExpressionSequence(expr_tensor = attn_out)
+
+  def initial_state(self):
+    return PseudoState(self)
+
 class HarwathSpeechEncoder(Encoder, Serializable):
   yaml_tag = u'!HarwathSpeechEncoder'
   def __init__(self, filter_height, filter_width, channels, num_filters, stride):
