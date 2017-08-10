373a02d0b23213e561c7e8a1aff8dd9b653061d8
Graham Neubig <neubig@gmail.com>
2017-08-10 03:30:34 -0400

    Fixed padding in harwath encoder

--- test/xnmt_test.py
+++ test/xnmt_test.py
@@ -1,10 +1,13 @@
 import sys
-import dynet
+import dynet as dy
 import random
 import unittest
 
 import batcher
 import input
+import specialized_encoders
+import expression_sequence
+import model_globals
 
 class TestBatcher(unittest.TestCase):
 
@@ -33,6 +36,20 @@ class TestBatcher(unittest.TestCase):
     self.assertEqual([[0, 0, 0]], [x.words for x in trg[3]])
     self.assertEqual([[0, 0, 0, 0, 0, 0]], [x.words for x in src[4]])
     self.assertEqual([[0, 0, 0, 0]], [x.words for x in trg[4]])
+
+class TestSpecializedEncoders(unittest.TestCase):
+
+  def test_harwath_speech_works_on_short_inputs(self):
+    model_globals.dynet_param_collection = model_globals.PersistentParamCollection("/tmp/model", 1)
+
+    enc = specialized_encoders.HarwathSpeechEncoder(
+                 filter_height = [2, 1, 1],
+                 filter_width = [5, 25, 25],
+                 channels = [1, 4, 6],
+                 num_filters = [4, 6, 8],
+                 stride= [1, 1, 1])
+    expseq = expression_sequence.ExpressionSequence(expr_list = [dy.zeroes([2])])
+    enc.transduce(expseq)
     
 if __name__ == '__main__':
   unittest.main()
