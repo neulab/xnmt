4689ba6dc18e927500a2177224b97ecba1ef5fca
philip30 <philip.arthur30@gmail.com>
2017-09-21 17:31:34 -0400

    Retab

--- xnmt/cython/src/functions.cpp
+++ xnmt/cython/src/functions.cpp
@@ -26,62 +26,62 @@ size_t deque_hash (const deque<int> vec) {
 
 NGramStats calculate_stats(const vector<int>& sents, unsigned int ngram, int eos_sym) {
   NGramStats stat;
-	deque<int> buffer;
-	for (size_t i=0; i < ngram; ++i) {
-		unordered_map<size_t, int> current_map;	
-		for (const int word : sents) {
-			if (word == eos_sym) {
-				break;
-			}
-			buffer.push_back(word);
-			size_t size = buffer.size();
-			if (size == ngram) {
-				size_t hash = deque_hash(buffer);
-				auto it = current_map.find(hash);
-				if (it == current_map.end()) {
-					current_map.insert(std::make_pair(hash, 0));
-				} else {
-					current_map.insert(std::make_pair(hash, it->second+1));
-				}
-				buffer.pop_front();
-			}	
-		}
-		stat.push_back(current_map);
-	}
+  deque<int> buffer;
+  for (size_t i=0; i < ngram; ++i) {
+    unordered_map<size_t, int> current_map; 
+    for (const int word : sents) {
+      if (word == eos_sym) {
+        break;
+      }
+      buffer.push_back(word);
+      size_t size = buffer.size();
+      if (size == ngram) {
+        size_t hash = deque_hash(buffer);
+        auto it = current_map.find(hash);
+        if (it == current_map.end()) {
+          current_map.insert(std::make_pair(hash, 0));
+        } else {
+          current_map.insert(std::make_pair(hash, it->second+1));
+        }
+        buffer.pop_front();
+      } 
+    }
+    stat.push_back(current_map);
+  }
   return stat;
 }
 
 double evaluate_bleu_sentence(const vector<int>& ref, const vector<int>& hyp,
                               int ngram, int smooth, int eos_sym) {
   NGramStats ref_stat = calculate_stats(ref, ngram, eos_sym);
-	NGramStats hyp_stat = calculate_stats(hyp, ngram, eos_sym);
+  NGramStats hyp_stat = calculate_stats(hyp, ngram, eos_sym);
   double log_precision = 0;
-	double log_bp = 0;
-	for (int i=0; i < ngram; ++i) {
-		auto ref_stat_i = ref_stat[i];
-		auto hyp_stat_i = hyp_stat[i];
-		int tp = 0;
-		int denom = 0;
-		for (auto it=ref_stat_i.begin(); it != ref_stat_i.end(); ++it) {
-			int ref_count = it->second;
-			int hyp_count = 0;
-			auto hyp_it = hyp_stat_i.find(it->first);
-			if (hyp_it != hyp_stat_i.end()) {
-				hyp_count = hyp_it->second;
-			}
-			tp += std::min(ref_count, hyp_count);
-			denom += hyp_count;
-		}
-		log_precision += log(static_cast<double>(tp + smooth) / (denom + smooth));
-	}
-	int len_hyp = hyp.size();
-	int len_ref = ref.size();
-	if (len_ref != 0 and len_hyp < len_ref) {
-		log_bp = 1 - (static_cast<double>(len_ref) / len_hyp);
-	} else {
-		log_bp = 0;
-	}
-	return exp(log_precision / ngram + log_bp);
+  double log_bp = 0;
+  for (int i=0; i < ngram; ++i) {
+    auto ref_stat_i = ref_stat[i];
+    auto hyp_stat_i = hyp_stat[i];
+    int tp = 0;
+    int denom = 0;
+    for (auto it=ref_stat_i.begin(); it != ref_stat_i.end(); ++it) {
+      int ref_count = it->second;
+      int hyp_count = 0;
+      auto hyp_it = hyp_stat_i.find(it->first);
+      if (hyp_it != hyp_stat_i.end()) {
+        hyp_count = hyp_it->second;
+      }
+      tp += std::min(ref_count, hyp_count);
+      denom += hyp_count;
+    }
+    log_precision += log(static_cast<double>(tp + smooth) / (denom + smooth));
+  }
+  int len_hyp = hyp.size();
+  int len_ref = ref.size();
+  if (len_ref != 0 and len_hyp < len_ref) {
+    log_bp = 1 - (static_cast<double>(len_ref) / len_hyp);
+  } else {
+    log_bp = 0;
+  }
+  return exp(log_precision / ngram + log_bp);
 }
- 
+
 }  // namespace xnmt
