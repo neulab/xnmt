08fee0a7d03598e0855d8985c071c29739e1f5f9
Matthias Sperber <matthias.sperber@gmail.com>
2017-10-28 18:41:14 +0200

    fixed spaces

--- xnmt/events.py
+++ xnmt/events.py
@@ -55,60 +55,60 @@ handler_method_names = set()
 event_names = set()
 
 def register_handler(inst):
-    handler_instances.append(inst)
+  handler_instances.append(inst)
     
 def register_xnmt_event(f):
-    def wrapper(obj, *args, **kwargs):
-        assert handler_method_names <= event_names, "detected handler for non-existant event: {}".format(handler_method_names-event_names)
-        f(obj, *args, **kwargs)
-        for handler in handler_instances:
-            bound_handler = getattr(handler, "on_" + f.__name__, None)
-            if bound_handler:
-                ret = bound_handler(*args, **kwargs)
-                if type(ret)!=tuple or len(ret)!=2 or ret[1][3:]!=f.__name__:
-                    raise RuntimeError("attempted to call unregistered handler {}".format(f.__name__))
-    event_names.add(f.__name__)
-    return wrapper
+  def wrapper(obj, *args, **kwargs):
+    assert handler_method_names <= event_names, "detected handler for non-existant event: {}".format(handler_method_names-event_names)
+    f(obj, *args, **kwargs)
+    for handler in handler_instances:
+      bound_handler = getattr(handler, "on_" + f.__name__, None)
+      if bound_handler:
+        ret = bound_handler(*args, **kwargs)
+        if type(ret)!=tuple or len(ret)!=2 or ret[1][3:]!=f.__name__:
+          raise RuntimeError("attempted to call unregistered handler {}".format(f.__name__))
+  event_names.add(f.__name__)
+  return wrapper
   
 def register_xnmt_event_assign(f):
-    def wrapper(obj, *args, **kwargs):
-        assert "context" in kwargs, "register_xnmt_event_assign requires \"context\" in kwargs"
-        assert handler_method_names <= event_names, "detected handler for non-existant event: {}".format(handler_method_names-event_names)
-        kwargs["context"] = f(obj, *args, **kwargs)
-        for handler in handler_instances:
-            bound_handler = getattr(handler, "on_" + f.__name__, None)
-            if bound_handler:
-                ret = bound_handler(*args, **kwargs)
-                if type(ret)!=tuple or len(ret)!=2 or ret[1][3:]!=f.__name__:
-                    raise RuntimeError("attempted to call unregistered handler {}".format(f.__name__))
-                kwargs["context"] = ret[0]
-        return kwargs["context"]
-    event_names.add(f.__name__)
-    return wrapper
+  def wrapper(obj, *args, **kwargs):
+    assert "context" in kwargs, "register_xnmt_event_assign requires \"context\" in kwargs"
+    assert handler_method_names <= event_names, "detected handler for non-existant event: {}".format(handler_method_names-event_names)
+    kwargs["context"] = f(obj, *args, **kwargs)
+    for handler in handler_instances:
+      bound_handler = getattr(handler, "on_" + f.__name__, None)
+      if bound_handler:
+        ret = bound_handler(*args, **kwargs)
+        if type(ret)!=tuple or len(ret)!=2 or ret[1][3:]!=f.__name__:
+          raise RuntimeError("attempted to call unregistered handler {}".format(f.__name__))
+        kwargs["context"] = ret[0]
+    return kwargs["context"]
+  event_names.add(f.__name__)
+  return wrapper
   
 def register_xnmt_event_sum(f):
-    def wrapper(obj, *args, **kwargs):
-        assert handler_method_names <= event_names, "detected handler for non-existant event: {}".format(handler_method_names-event_names)
-        res = f(obj, *args, **kwargs)
-        for handler in handler_instances:
-            bound_handler = getattr(handler, "on_" + f.__name__, None)
-            if bound_handler:
-                ret = bound_handler(*args, **kwargs)
-                if type(ret)!=tuple or len(ret)!=2 or ret[1][3:]!=f.__name__:
-                    raise RuntimeError("attempted to call unregistered handler {}".format(f.__name__))
-                tmp = ret[0]
-                if res is None: res = tmp
-                elif tmp is not None: res = res + tmp
-        return res
-    event_names.add(f.__name__)
-    return wrapper
+  def wrapper(obj, *args, **kwargs):
+    assert handler_method_names <= event_names, "detected handler for non-existant event: {}".format(handler_method_names-event_names)
+    res = f(obj, *args, **kwargs)
+    for handler in handler_instances:
+      bound_handler = getattr(handler, "on_" + f.__name__, None)
+      if bound_handler:
+        ret = bound_handler(*args, **kwargs)
+        if type(ret)!=tuple or len(ret)!=2 or ret[1][3:]!=f.__name__:
+          raise RuntimeError("attempted to call unregistered handler {}".format(f.__name__))
+        tmp = ret[0]
+        if res is None: res = tmp
+        elif tmp is not None: res = res + tmp
+    return res
+  event_names.add(f.__name__)
+  return wrapper
   
 def handle_xnmt_event(f):
-    def wrapper(obj, *args, **kwargs):
-        return f(obj, *args, **kwargs), f.__name__
-    assert f.__name__.startswith("on_"), "xnmt event handlers must be named on_*, found {}".format(f.__name__)
-    handler_method_names.add(f.__name__[3:])
-    return wrapper
+  def wrapper(obj, *args, **kwargs):
+    return f(obj, *args, **kwargs), f.__name__
+  assert f.__name__.startswith("on_"), "xnmt event handlers must be named on_*, found {}".format(f.__name__)
+  handler_method_names.add(f.__name__[3:])
+  return wrapper
   
 class GeneratorModel(object):
   def generate_output(self, *args, **kwargs):
