d1330cf830bb22a82ac33a0d60336ee39182a0cf
Matthias Sperber <matthias.sperber@gmail.com>
2017-10-28 09:30:06 +0200

    Revert "disabled enum for debugging"
    This reverts commit 35b521511ff3b5b1b863b10227bd82631cfab0b2.

--- xnmt/segmenting_encoder.py
+++ xnmt/segmenting_encoder.py
@@ -5,7 +5,7 @@ import six
 import numpy
 import dynet as dy
 
-#from enum import Enum
+from enum import Enum
 from xml.sax.saxutils import escape, unescape
 from lxml import etree
 from scipy.stats import poisson
@@ -19,7 +19,7 @@ from xnmt.serializer import Serializable
 from xnmt.transducer import SeqTransducer, FinalTransducerState
 from xnmt.loss import LossBuilder
 
-class SegmentingAction(object): # TODO
+class SegmentingAction(Enum):
   """
   The enumeration of possible action.
   """
@@ -141,13 +141,13 @@ class SegmentingSeqTransducer(SeqTransducer, Serializable, Reportable):
       for i, decision in enumerate(segment_decision):
         # If segment for this particular input
         decision = int(decision)
-        if decision == SegmentingAction.DELETE: # TODO .value:
+        if decision == SegmentingAction.DELETE.value:
           continue
         # Get the particular encoding for that batch item
         encoding_i = dy.pick_batch_elem(encoding, i)
         # Append the encoding for this item to the buffer
         buffers[i].append(encoding_i)
-        if decision == SegmentingAction.SEGMENT: # TODO .value:
+        if decision == SegmentingAction.SEGMENT.value:
           expr_seq = expression_sequence.ExpressionSequence(expr_list=buffers[i])
           transduce_output = self.segment_transducer.transduce(expr_seq)
           outputs[i].append(transduce_output)
@@ -245,9 +245,9 @@ class SegmentingSeqTransducer(SeqTransducer, Serializable, Reportable):
     segmented = []
     temp = ""
     for decision, word in zip(segmentation, words):
-      if decision == SegmentingAction.READ: # TODO .value:
+      if decision == SegmentingAction.READ.value:
         temp += word
-      elif decision == SegmentingAction.SEGMENT: # TODO .value:
+      elif decision == SegmentingAction.SEGMENT.value:
         temp += word
         segmented.append((temp, False))
         temp = ""
