c699522275eb1ae5c138fe7207a93f0ffe69db3e
Roee Aharoni <aharonr6@nlp.lnx.biu.ac.il>
2017-12-04 17:26:26 +0200

    fix merge mess

--- xnmt/evaluator.py
+++ xnmt/evaluator.py
@@ -450,7 +450,6 @@ class ExternalEvaluator(object):
     """
     proc = subprocess.Popen([self.path], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
-    print("external eval script score: {}".format(out))
     external_score = float(out)
     return ExternalScore(external_score, self.higher_better)
 
