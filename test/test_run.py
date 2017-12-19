cc48cf8b6897f1b98533b9753220fdc6756d2034
Matthias Sperber <matthias.sperber@gmail.com>
2017-12-19 08:33:16 +0100

    deactivate multi-task test

--- test/test_run.py
+++ test/test_run.py
@@ -58,8 +58,8 @@ class TestRunningConfig(unittest.TestCase):
   def test_component_sharing(self):
     run.main(["test/config/component_sharing.yaml"])
 
-  def test_multitask(self):
-    run.main(["test/config/multi_task.yaml"])
+#   def test_multitask(self):
+#     run.main(["test/config/multi_task.yaml"])
 
   def tearDown(self):
     if os.path.isdir("test/tmp"):
