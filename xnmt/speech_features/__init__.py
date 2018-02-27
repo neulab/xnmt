99cee8c71f8927e0ecd27fbc693ba6e9d63f598b
Matthias Sperber <matthias.sperber@gmail.com>
2018-02-27 09:25:10 +0100

    added speech-features dependencies

---
+++ xnmt/speech_features/__init__.py
@@ -0,0 +1,4 @@
+# this is copied from https://github.com/thomasZen/python_speech_features2,
+# which is itself based on https://github.com/jameslyons/python_speech_features
+# and https://github.com/ZitengWang/python_kaldi_features
+from .base import *
