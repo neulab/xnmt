26a4e634a8bf2f6c0ca471cb40e3293ce4eab0e2
Philip Arthur <philip30@users.noreply.github.com>
2018-03-02 17:09:54 +0900

    Removed support for python2 (#283)

--- xnmt/__init__.py
+++ xnmt/__init__.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+# No support for python2
+if sys.version_info[0] == 2:
+  raise RuntimeError("XNMT does not support python2 any longer.")
+
 package_dir = os.path.dirname(os.path.abspath(__file__))
 if package_dir not in sys.path:
   sys.path.append(package_dir)
