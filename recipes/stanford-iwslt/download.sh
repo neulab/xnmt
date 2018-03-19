3f3421bd8a5f7c161e77e0b61e9a67194e20bf4c
Matthias Sperber <matthias.sperber@gmail.com>
2018-03-19 15:22:10 +0100

    stanford recipe

---
+++ recipes/stanford-iwslt/download.sh
@@ -0,0 +1,8 @@
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/train.en
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/train.vi
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/tst2012.en
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/tst2012.vi
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/tst2013.en
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/tst2013.vi
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/vocab.en
+wget https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi/vocab.vi
