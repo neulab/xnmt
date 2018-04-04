6004ea0e361fc38eff9fd9e7669a2c6e5268baed
Matthias Sperber <matthias.sperber@gmail.com>
2018-04-04 18:52:46 +0200

    enable zero-evaluation (before training starts)

--- xnmt/experiment.py
+++ xnmt/experiment.py
@@ -22,6 +22,7 @@ class Experiment(Serializable):
     exp_global: global experiment settings
     preproc: carry out preprocessing if specified
     model: The main model. In the case of multitask training, several models must be specified, in which case the models will live not here but inside the training task objects.
+    eval_zero: list of tasks to evaluate before the training starts (useful e.g. with pretrained models).
     train: The training regimen defines the training loop.
     evaluate: list of tasks to evaluate the model after training finishes.
     random_search_report: When random search is used, this holds the settings that were randomly drawn for documentary purposes.
@@ -34,12 +35,14 @@ class Experiment(Serializable):
                exp_global:Optional[ExpGlobal] = bare(ExpGlobal),
                preproc:Optional[PreprocRunner] = None,
                model:Optional[GeneratorModel] = None,
+               eval_zero:Optional[List[EvalTask]] = None,
                train:Optional[TrainingRegimen] = None,
                evaluate:Optional[List[EvalTask]] = None,
                random_search_report:Optional[dict] = None) -> None:
     self.exp_global = exp_global
     self.preproc = preproc
     self.model = model
+    self.eval_zero = eval_zero
     self.train = train
     self.evaluate = evaluate
 
@@ -48,25 +51,32 @@ class Experiment(Serializable):
 
   def __call__(self, save_fct):
     """
-    Launch training loop, followed by final evaluation.
+    Launch eval_zero -> training loop -> final evaluation (all optional).
     """
-    eval_scores = "Not evaluated"
+    eval_zero_scores = self.run_evaluators(self.eval_zero, "> Performing zero evaluation")
+    if eval_zero_scores:
+      for i in range(len(eval_zero_scores)):
+        print(f"  [Zero score] {str(eval_zero_scores[i])}")
+
     if self.train:
       logger.info("> Training")
       self.train.run_training(save_fct = save_fct)
       logger.info('reverting learned weights to best checkpoint..')
       ParamManager.param_col.revert_to_best_model()
 
-    evaluate_args = self.evaluate
-    if evaluate_args:
-      logger.info("> Performing final evaluation")
+    eval_scores = self.run_evaluators(self.evaluate, "> Performing final evaluation") or "Not evaluated"
+    return eval_scores
+
+  def run_evaluators(self, evaluators, log_str):
+    eval_scores = None
+    if evaluators:
+      logger.info(log_str)
       eval_scores = []
-      for evaluator in evaluate_args:
+      for evaluator in evaluators:
         eval_score, _ = evaluator.eval()
         if type(eval_score) == list:
           eval_scores.extend(eval_score)
         else:
           eval_scores.append(eval_score)
-
     return eval_scores
 
