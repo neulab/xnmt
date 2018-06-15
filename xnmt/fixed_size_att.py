fccb65289f20549ac152e057bdb2100c94ef551c
Matthias Sperber <matthias.sperber@gmail.com>
2018-06-15 23:01:33 +0200

    precompute pos encodings

--- xnmt/fixed_size_att.py
+++ xnmt/fixed_size_att.py
@@ -30,12 +30,19 @@ class FixedSizeAttSeqTransducer(transducer.SeqTransducer, Serializable):
                output_len: int = 32,
                pos_enc_max: Optional[int] = None,
                param_init: xnmt.param_init.ParamInitializer = Ref("exp_global.param_init",
-                                                                  default=bare(xnmt.param_init.GlorotInitializer))):
+                                                                  default=bare(xnmt.param_init.GlorotInitializer))) \
+          -> None:
     subcol = param_collection.ParamManager.my_params(self)
     self.output_len = output_len
     self.W = subcol.add_parameters(dim=(hidden_dim, output_len),
                                    init=param_init.initializer((hidden_dim, output_len)))
     self.pos_enc_max = pos_enc_max
+    if self.pos_enc_max:
+      self.pos_enc = np.zeros((self.pos_enc_max, self.output_len))
+      for k in range(self.output_len):
+        for s in range(self.pos_enc_max):
+          self.pos_enc[s, k] = (1.0 - k / self.output_len) * (
+                  1.0 - s / self.pos_enc_max) + k / self.output_len * s / self.pos_enc_max
 
   def __call__(self, x):
     x_T = x.as_transposed_tensor()
@@ -44,11 +51,7 @@ class FixedSizeAttSeqTransducer(transducer.SeqTransducer, Serializable):
       scores = x.mask.add_to_tensor_expr(scores, multiplicator=-100.0, time_first=True)
     if self.pos_enc_max:
       seq_len = x_T.dim()[0][0]
-      pos_enc = np.zeros((seq_len, self.output_len))
-      for k in range(self.output_len):
-        for s in range(seq_len):
-          pos_enc[s, k] = (1.0 - k / self.output_len) * (
-                    1.0 - s / self.pos_enc_max) + k / self.output_len * s / self.pos_enc_max
+      pos_enc = self.pos_enc[:seq_len,:]
       scores = dy.cmult(scores, dy.inputTensor(pos_enc))
     attention = dy.softmax(scores)
     output_expr = x.as_tensor() * attention
