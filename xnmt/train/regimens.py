07a814515e81eca15ce301881105c16381afd682
Daniel Beck <daniel.e.beck@gmail.com>
2018-10-16 16:36:06 +1100

    first try

--- xnmt/train/regimens.py
+++ xnmt/train/regimens.py
@@ -181,6 +181,148 @@ class SimpleTrainingRegimen(train_tasks.SimpleTrainingTask, TrainingRegimen, Ser
       assert 0 < self.num_updates_skipped < self.update_every
 
 
+class AutobatchTrainingRegimen(train_tasks.SimpleTrainingTask, TrainingRegimen, Serializable):
+  """
+  Args:
+    model: the model
+    src_file: the source training file
+    trg_file: the target training file
+    dev_every: dev checkpoints every n sentences (0 for only after epoch)
+    dev_zero: if True, add a checkpoint before training loop is entered (useful with pretrained models).
+    batcher: Type of batcher
+    loss_calculator: The method for calculating the loss.
+    trainer: Trainer object, default is SGD with learning rate 0.1
+    run_for_epochs:
+    lr_decay:
+    lr_decay_times:  Early stopping after decaying learning rate a certain number of times
+    patience: apply LR decay after dev scores haven't improved over this many checkpoints
+    initial_patience: if given, allows adjusting patience for the first LR decay
+    dev_tasks: A list of tasks to use during the development stage.
+    dev_combinator: A formula to combine together development scores into a single score to
+                    choose whether to perform learning rate decay, etc.
+                    e.g. 'x[0]-x[1]' would say that the first dev task score minus the
+                    second dev task score is our measure of how good we're doing. If not
+                    specified, only the score from the first dev task will be used.
+    restart_trainer: Restart trainer (useful for Adam) and revert weights to best dev checkpoint when applying
+                            LR decay (https://arxiv.org/pdf/1706.09733.pdf)
+    reload_command: Command to change the input data after each epoch.
+                         --epoch EPOCH_NUM will be appended to the command.
+                         To just reload the data after each epoch set the command to ``True``.
+    name: will be prepended to log outputs if given
+    sample_train_sents:
+    max_num_train_sents:
+    max_src_len:
+    max_trg_len:
+    loss_comb_method: method for combining loss across batch elements (``sum`` or ``avg``).
+    update_every: simulate large-batch training by accumulating gradients over several steps before updating parameters
+    commandline_args:
+  """
+  yaml_tag = '!AutobatchTrainingRegimen'
+
+  @serializable_init
+  def __init__(self,
+               model: ConditionedModel = Ref("model"),
+               src_file: Union[None, str, Sequence[str]] = None,
+               trg_file: Optional[str] = None,
+               dev_every: numbers.Integral = 0,
+               dev_zero: bool = False,
+               batcher: batchers.Batcher = bare(batchers.SrcBatcher, batch_size=32),
+               loss_calculator: LossCalculator = bare(MLELoss),
+               trainer: optimizers.XnmtOptimizer = bare(optimizers.SimpleSGDTrainer, e0=0.1),
+               run_for_epochs: Optional[numbers.Integral] = None,
+               lr_decay: numbers.Real= 1.0,
+               lr_decay_times: numbers.Integral = 3,
+               patience: numbers.Integral = 1,
+               initial_patience: Optional[numbers.Integral] = None,
+               dev_tasks: Sequence[eval_tasks.EvalTask] = None,
+               dev_combinator: Optional[str] = None,
+               restart_trainer: bool = False,
+               reload_command: Optional[str] = None,
+               name: str = "{EXP}",
+               sample_train_sents: Optional[numbers.Integral] = None,
+               max_num_train_sents: Optional[numbers.Integral] = None,
+               max_src_len: Optional[numbers.Integral] = None,
+               max_trg_len: Optional[numbers.Integral] = None,
+               loss_comb_method: str = Ref("exp_global.loss_comb_method", default="sum"),
+               update_every: numbers.Integral = 1,
+               commandline_args: dict = Ref("exp_global.commandline_args", default={})) -> None:
+
+    super().__init__(model=model,
+                     src_file=src_file,
+                     trg_file=trg_file,
+                     dev_every=dev_every,
+                     batcher=batcher,
+                     loss_calculator=loss_calculator,
+                     run_for_epochs=run_for_epochs,
+                     lr_decay=lr_decay,
+                     lr_decay_times=lr_decay_times,
+                     patience=patience,
+                     initial_patience=initial_patience,
+                     dev_tasks=dev_tasks,
+                     dev_combinator=dev_combinator,
+                     restart_trainer=restart_trainer,
+                     reload_command=reload_command,
+                     name=name,
+                     sample_train_sents=sample_train_sents,
+                     max_num_train_sents=max_num_train_sents,
+                     max_src_len=max_src_len,
+                     max_trg_len=max_trg_len)
+    self.dev_zero = dev_zero
+    self.trainer = trainer or optimizers.SimpleSGDTrainer(e0=0.1)
+    self.dynet_profiling = commandline_args.get("dynet_profiling", 0) if commandline_args else 0
+    self.train_loss_tracker = TrainLossTracker(self)
+    self.loss_comb_method = loss_comb_method
+    self.update_every = update_every
+    self.num_updates_skipped = 0
+
+  def run_training(self, save_fct):
+    """
+    Main training loop (overwrites TrainingRegimen.run_training())
+    """
+    if self.run_for_epochs is None or self.run_for_epochs > 0:
+      losses = []
+      for src, trg in self.next_minibatch():
+        if self.dev_zero:
+          self.checkpoint_and_save(save_fct)
+          self.dev_zero = False
+        with utils.ReportOnException({"src": src, "trg": trg, "graph": utils.print_cg_conditional}):
+          #####
+          if self.num_updates_skipped == 0:
+            dy.renew_cg(immediate_compute=settings.IMMEDIATE_COMPUTE, check_validity=settings.CHECK_VALIDITY)
+          #####
+          with self.train_loss_tracker.time_tracker:
+            event_trigger.set_train(True)
+            loss_builder = self.training_step(src, trg)
+            loss = loss_builder.compute()
+            ######
+            #self.backward(loss, self.dynet_profiling)
+            losses.append(loss)
+            #self.update(self.trainer)
+            self.update(self.trainer, losses)
+            if self.num_updates_skipped == 0:
+              losses = []
+            ######
+          self.train_loss_tracker.report(trg, loss_builder.get_factored_loss_val(comb_method=self.loss_comb_method))
+        if self.checkpoint_needed():
+          self.checkpoint_and_save(save_fct)
+        if self.should_stop_training(): break
+
+  def checkpoint_and_save(self, save_fct):
+    should_save = self.checkpoint()
+    if should_save:
+      save_fct()
+
+  def update(self, trainer: optimizers.XnmtOptimizer,
+             losses) -> None:
+    self.num_updates_skipped += 1
+    if self.num_updates_skipped == self.update_every:
+      self.backward(sum(losses), self.dynet_profiling)
+      trainer.update()
+      self.num_updates_skipped = 0
+    else:
+      assert 0 < self.num_updates_skipped < self.update_every
+
+
 class MultiTaskTrainingRegimen(TrainingRegimen):
   """
   Base class for multi-task training classes.
