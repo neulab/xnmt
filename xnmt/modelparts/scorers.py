29b342915e2c3390a6c8e5b058e575b93a7019bf
Philip Arthur <philip.arthur30@gmail.com>
2018-11-20 16:38:45 +1100

    Refactor lexicon softmax

--- xnmt/modelparts/scorers.py
+++ xnmt/modelparts/scorers.py
@@ -3,9 +3,10 @@ import numbers
 
 import dynet as dy
 
-from xnmt import batchers, input_readers, param_collections, param_initializers, vocabs
+from xnmt import batchers, input_readers, param_collections, param_initializers, vocabs, logger
 from xnmt.modelparts import transforms
 from xnmt.persistence import Serializable, serializable_init, bare, Ref
+from xnmt.events import handle_xnmt_event, register_xnmt_handler
 
 class Scorer(object):
   """
@@ -126,12 +127,13 @@ class Softmax(Scorer, Serializable):
   
   def calc_scores(self, x: dy.Expression) -> dy.Expression:
     return self.output_projector.transform(x)
+  
+  def is_modifying_softmax_layer(self):
+    return self.label_smoothing > 0.0
 
   def calc_loss(self, x: dy.Expression, y: Union[numbers.Integral, List[numbers.Integral]]) -> dy.Expression:
-
-    scores = self.calc_scores(x)
-
-    if self.label_smoothing == 0.0:
+    if not self.is_modifying_softmax_layer():
+      scores = self.calc_scores(x)
       # single mode
       if not batchers.is_batched(y):
         loss = dy.pickneglogsoftmax(scores, y)
@@ -139,14 +141,15 @@ class Softmax(Scorer, Serializable):
       else:
         loss = dy.pickneglogsoftmax_batch(scores, y)
     else:
-      log_prob = dy.log_softmax(scores)
+      log_prob = self.calc_log_probs(x)
       if not batchers.is_batched(y):
-        pre_loss = -dy.pick(log_prob, y)
+        loss = -dy.pick(log_prob, y)
       else:
-        pre_loss = -dy.pick_batch(log_prob, y)
+        loss = -dy.pick_batch(log_prob, y)
 
-      ls_loss = -dy.mean_elems(log_prob)
-      loss = ((1 - self.label_smoothing) * pre_loss) + (self.label_smoothing * ls_loss)
+      if self.label_smoothing > 0:
+        ls_loss = -dy.mean_elems(log_prob)
+        loss = ((1 - self.label_smoothing) * loss) + (self.label_smoothing * ls_loss)
     
     return loss
 
@@ -156,3 +159,150 @@ class Softmax(Scorer, Serializable):
   def calc_log_probs(self, x: dy.Expression) -> dy.Expression:
     return dy.log_softmax(self.calc_scores(x))
 
+
+class LexiconSoftmax(Softmax, Serializable):
+  """
+    A subclass of the softmax class but can make use of a external lexicon probability scorer as defined in:
+    http://anthology.aclweb.org/D/D16/D16-1162.pdf
+
+    Args:
+      input_dim: Size of the input vector
+      vocab_size: Size of the vocab to predict
+      vocab: A vocab object from which the vocab size can be derived automatically
+      trg_reader: An input reader for the target, which can be used to derive the vocab size
+      label_smoothing: Whether to apply label smoothing (a value of 0.1 is good if so)
+      param_init: How to initialize the parameters
+      bias_init: How to initialize the bias
+      output_projector: The projection to be used before the output
+    """
+  
+  yaml_tag = '!LexiconSoftmax'
+
+  @serializable_init
+  @register_xnmt_handler
+  def __init__(self,
+               input_dim: numbers.Integral = Ref("exp_global.default_layer_dim"),
+               vocab_size: Optional[numbers.Integral] = None,
+               vocab: Optional[vocabs.Vocab] = None,
+               trg_reader: Optional[input_readers.InputReader] = Ref("model.trg_reader", default=None),
+               attender = Ref("model.attender"),
+               label_smoothing: numbers.Real = 0.0,
+               param_init: param_initializers.ParamInitializer = Ref("exp_global.param_init", default=bare(
+                 param_initializers.GlorotInitializer)),
+               bias_init: param_initializers.ParamInitializer = Ref("exp_global.bias_init",
+                                                                    default=bare(param_initializers.ZeroInitializer)),
+               output_projector: transforms.Linear = None,
+               lexicon_file=None,
+               lexicon_alpha=0.001,
+               lexicon_type='bias',
+               coef_predictor: transforms.Linear = None,
+               src_vocab = Ref("model.src_reader.vocab", default=None)) -> None:
+    self.param_col = param_collections.ParamManager.my_params(self)
+    self.input_dim = input_dim
+    self.output_dim = self._choose_vocab_size(vocab_size, vocab, trg_reader)
+    self.label_smoothing = label_smoothing
+  
+    self.output_projector = self.add_serializable_component("output_projector", output_projector,
+                                                            lambda: output_projector or transforms.Linear(
+                                                              input_dim=self.input_dim, output_dim=self.output_dim,
+                                                              param_init=param_init, bias_init=bias_init))
+    self.coef_predictor = self.add_serializable_component("coef_predictor", coef_predictor,
+                                                          lambda: coef_predictor or transforms.Linear(
+                                                            input_dim=self.input_dim, output_dim=1,
+                                                            param_init=param_init, bias_init=bias_init
+                                                          ))
+    self.lexicon_file = lexicon_file
+    self.lexicon_type = lexicon_type
+    self.lexicon_alpha = lexicon_alpha
+    
+    assert lexicon_type in ["bias", "linear"], "Lexicon type can be either 'bias' or 'linear' only!"
+    # Reference to other parts of the model
+    self.src_vocab = src_vocab
+    self.trg_vocab = vocab if vocab is not None else trg_reader.vocab
+    self.attender = attender
+    # Sparse data structure to store exteranl lexicon prob
+    self.lexicon = None
+    # State of the sofmax
+    self.lexicon_prob = None
+    self.coeff = None
+    self.dict_prob = None
+    
+  def load_lexicon(self):
+    logger.info("Loading lexicon from file: " + self.lexicon_file)
+    lexicon = [{} for _ in range(len(self.src_vocab))]
+    with open(self.lexicon_file, encoding='utf-8') as fp:
+      for line in fp:
+        try:
+          trg, src, prob = line.rstrip().split()
+        except:
+          logger.warning("Failed to parse 'trg src prob' from:" + line.strip())
+          continue
+        trg_id = self.trg_vocab.convert(trg)
+        src_id = self.src_vocab.convert(src)
+        lexicon[src_id][trg_id] = float(prob)
+    # Setting the rest of the weight to the unknown word
+    for i in range(len(lexicon)):
+      sum_prob = sum(lexicon[i].values())
+      if sum_prob < 1.0:
+        lexicon[i][self.trg_vocab.convert(self.trg_vocab.unk_token)] = 1.0 - sum_prob
+    # Overriding special tokens
+    src_unk_id = self.src_vocab.convert(self.src_vocab.unk_token)
+    trg_unk_id = self.trg_vocab.convert(self.trg_vocab.unk_token)
+    lexicon[self.src_vocab.SS] = {self.trg_vocab.SS: 1.0}
+    lexicon[self.src_vocab.ES] = {self.trg_vocab.ES: 1.0}
+    # TODO(philip30): Note sure if this is intended
+    lexicon[src_unk_id] = {trg_unk_id: 1.0}
+    return lexicon
+
+  @handle_xnmt_event
+  def on_new_epoch(self, *args, **kwargs):
+    if self.lexicon is None:
+      self.lexicon = self.load_lexicon()
+  
+  @handle_xnmt_event
+  def on_start_sent(self, src):
+    self.coeff = None
+    self.dict_prob = None
+    
+    batch_size = src.batch_size()
+    col_size = src.sent_len()
+  
+    idxs = [(x, j, i) for i in range(batch_size) for j in range(col_size) for x in self.lexicon[src[i][j]].keys()]
+    idxs = tuple(map(list, list(zip(*idxs))))
+  
+    values = [x for i in range(batch_size) for j in range(col_size) for x in self.lexicon[src[i][j]].values()]
+    dim = len(self.trg_vocab), col_size, batch_size
+    self.lexicon_prob = dy.nobackprop(dy.sparse_inputTensor(idxs, values, dim, batched=True))
+  
+  def calc_scores(self, x: dy.Expression) -> dy.Expression:
+    model_score = self.output_projector.transform(x)
+    if self.lexicon_type == 'bias':
+      model_score += dy.sum_dim(dy.log(self.calculate_dict_prob() + self.lexicon_alpha), [1])
+    return model_score
+  
+  def calculate_coeff(self, x):
+    if self.coeff is None:
+      self.coeff = dy.logistic(self.coef_predictor.transform(x))
+    return self.coeff
+  
+  def calculate_dict_prob(self):
+    if self.dict_prob is None:
+      self.dict_prob = self.lexicon_prob * self.attender.get_last_attention()
+    return self.dict_prob
+  
+  def calc_probs(self, x: dy.Expression) -> dy.Expression:
+    model_score = dy.softmax(self.calc_scores(x))
+    if self.lexicon_type == 'linear':
+      coeff = self.calculate_coeff(x)
+      return dy.sum_dim(dy.cmult(coeff, model_score) + dy.cmult((1-coeff), self.calculate_dict_prob()), [1])
+    else:
+      return model_score
+
+  def calc_log_probs(self, x: dy.Expression) -> dy.Expression:
+    if self.lexicon_type == 'linear':
+      return dy.log(self.calc_probs(x))
+    else:
+      return dy.log_softmax(self.calc_scores(x))
+
+  def is_modifying_softmax_layer(self):
+    return self.lexicon_type == 'linear' or super().is_modifying_softmax_layer()
