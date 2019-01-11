Fixed policy gradient to work with simult reward

xnmt/simultaneous/rewards.py
@@ -1,3 +1,9 @@
+
+import numpy as np
+import dynet as dy
+
+import xnmt.thirdparty.dl4mt_simul_trans.reward as simult_reward
+
 class SimultaneousReward(object):
   def __init__(self, src_batch, trg_batch, actions, outputs, trg_vocab):
     self.src_batch = src_batch
@@ -7,10 +13,11 @@ class SimultaneousReward(object):
     self.outputs = outputs
     
   def calculate(self):
+    ret = []
     for inp, ref, action, output in zip(self.src_batch, self.trg_batch, self.actions, self.outputs):
-      print(action)
-      print(output)
-      print(len(self.trg_vocab.i2w))
-      print(" ".join([self.trg_vocab[x] for x in output]))
-      print("--------------")
+      reward, bleu, delay, instant_reward = simult_reward.return_reward(output, ref, action, inp.len_unpadded()+1)
+      ret.append(reward)
+    return [dy.scalarInput(x) for x in np.hstack(ret)]
+  
+  
 