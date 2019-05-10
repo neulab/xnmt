c7175c15881fc1e46ccecb560b5ba005cae5e95c
Matthias Sperber <sperber@apple.com>
2019-05-10 17:03:39 +0200

    more work on manual tests

--- test/test_training_manual.py
+++ test/test_training_manual.py
@@ -63,6 +63,32 @@ class ManualTestingBaseClass(object):
       val = val.T
     np.testing.assert_almost_equal(actual_grads, val, decimal=places)
 
+  def assert_trained_mlp_att_grads(self, val, places, epochs=1, *args, **kwargs):
+    training_regimen = self.run_training(epochs=epochs-1, *args, **kwargs)
+    # last epoch is done manually and without calling update():
+    src, trg = next(training_regimen.next_minibatch())
+    tt.reset_graph()
+    event_trigger.set_train(True)
+    loss_builder = training_regimen.training_step(src, trg)
+    loss = loss_builder.compute(comb_method=training_regimen.loss_comb_method)
+    training_regimen.backward(loss)
+    # importantly: no update() here because that would zero out the dynet gradients
+
+    if xnmt.backend_dynet:
+      actual_grads = training_regimen.model.attender.pW.grad_as_array(), \
+                     training_regimen.model.attender.pV.grad_as_array(), \
+                     training_regimen.model.attender.pb.grad_as_array(), \
+                     training_regimen.model.attender.pU.grad_as_array()
+    else:
+      actual_grads = tt.npvalue(training_regimen.model.attender.linear_context._parameters['weight'].grad).T, \
+                     tt.npvalue(training_regimen.model.attender.linear_query._parameters['weight'].grad).T, \
+                     tt.npvalue(training_regimen.model.attender.linear_query._parameters['bias'].grad).T, \
+                     tt.npvalue(training_regimen.model.attender.pU._parameters['weight'].grad).T,
+    np.testing.assert_almost_equal(actual_grads[0], val[0], decimal=places)
+    np.testing.assert_almost_equal(actual_grads[1], val[1], decimal=places)
+    np.testing.assert_almost_equal(actual_grads[2], val[2], decimal=places)
+    np.testing.assert_almost_equal(actual_grads[3].flatten(), val[3].flatten(), decimal=places)
+
   def convert_pytorch_lstm_weights(self, weights):
     # change ifgo -> ifog; subtract 1-initialized forget gates
     h_dim = weights[0].shape[1] // 4
@@ -179,13 +205,14 @@ class TestManualFullLAS(unittest.TestCase, ManualTestingBaseClass):
     np.testing.assert_almost_equal(trained_decoder[0], val['decoder'][0], decimal=places)
     np.testing.assert_almost_equal(trained_decoder[1], val['decoder'][1], decimal=places)
     np.testing.assert_almost_equal(trained_decoder[2], val['decoder'][2], decimal=places)
+    # below 3 are mismatched:
     np.testing.assert_almost_equal(trained_attender[0], val['attender'][0], decimal=places)
     np.testing.assert_almost_equal(trained_attender[1], val['attender'][1], decimal=places)
     np.testing.assert_almost_equal(trained_attender[2], val['attender'][2], decimal=places)
     np.testing.assert_almost_equal(trained_attender[3].flatten(), val['attender'][3].flatten(), decimal=places)
 
-  def run_training(self, epochs=1, lr=0.1):
-    # TODO: label_smoothing: 0.1, AdamTrainer, lr_decay, restart_trainer
+  def run_training(self, epochs=1, lr=0.1, adam=False):
+    # TODO: AdamTrainer, lr_decay, restart_trainer
     layer_dim = 2
     batcher = SrcBatcher(batch_size=2, break_ties_randomly=False, pad_src_to_multiple=4)
     train_args = {}
@@ -288,9 +315,15 @@ class TestManualFullLAS(unittest.TestCase, ManualTestingBaseClass):
       )
     train_args['dev_tasks'] = []
     if xnmt.backend_dynet:
-      train_args['trainer'] = optimizers.SimpleSGDTrainer(e0=lr, clip_grads=-1)
+      if adam:
+        train_args['trainer'] = optimizers.AdamTrainer(alpha=lr*0.01, clip_grads=-1)
+      else:
+        train_args['trainer'] = optimizers.SimpleSGDTrainer(e0=lr, clip_grads=-1)
     else:
-      train_args['trainer'] = optimizers.SimpleSGDTrainer(e0=lr, clip_grads=0, rescale_grads=False)
+      if adam:
+        train_args['trainer'] = optimizers.AdamTrainer(alpha=lr * 0.01, clip_grads=0, rescale_grads=False)
+      else:
+        train_args['trainer'] = optimizers.SimpleSGDTrainer(e0=lr, clip_grads=0, rescale_grads=False)
     train_args['batcher'] = batcher
     train_args['run_for_epochs'] = epochs
     train_args['train_loss_tracker'] = TrainLossTracker(accumulative=True)
@@ -304,43 +337,99 @@ class TestManualFullLAS(unittest.TestCase, ManualTestingBaseClass):
   #   self.assert_loss_value(9.657503128051758, places=5)
   #
   # def test_loss_three_epochs(self):
-  #   self.assert_loss_value(8.524263381958008, places=2, epochs=3, lr=10)
+  #   self.assert_loss_value(8.524262428283691, places=2, epochs=3, lr=10)
+  #
+  # def test_mlp_att_grads(self):
+  #   expected = (np.asarray([[-3.15510178e-08,-3.91636625e-08],[-6.31020356e-08,-7.83273251e-08]]),
+  #               np.asarray([[-1.15501116e-10,1.41344131e-10],[-2.31002231e-10,2.82688262e-10]]),
+  #               np.asarray([1.65015179e-09,3.30030359e-09]),
+  #               np.asarray([[-1.09920165e-07,1.09920165e-07]]))
+  #   self.assert_trained_mlp_att_grads(expected, places=7, epochs=1, adam=True)
 
-  def test_all_params_one_epoch(self):
-    # useful regex: (?<!\[)\s+      ->      ,
-    expected = {
-      'trg_emb': np.asarray([[-0.08201125,0.11798876],[-0.2,0.2,],[-0.30986509,0.29013494],[-0.41663632,0.38336369],[-0.5,0.5,]]),
-      'transform': (np.asarray([[-0.11458226,-0.1818178,-0.29989332,-0.39990845],[ 0.11458226,0.1818178,0.29989332,0.39990845]]),
-                    np.asarray([ 0.01091676,-0.01091676])),
-      'out': (np.asarray([[-0.05089095,0.05089095],[-0.2470859,0.2470859,],[-0.32606718,0.32606718],[-0.42551777,0.42551777],[-0.45043823,0.45043823]]),
-              np.asarray([-5.37809563,3.61097455,3.60002279,3.58904791,-5.42194843])),
-      'enc_l0_fwd': (np.asarray([[-0.09993633,-0.19993669],[-0.09990665,-0.19991051],[-0.09992233,-0.19992436],[-0.09978127,-0.1998006,]]),
-                     np.asarray([[-0.10001528],[-0.10002846],[-0.1000223,],[-0.10008691]]),
-                     np.asarray([-1.90493865e-05,-5.03285955e-05,-3.61094171e-05,-4.47910599e-04])),
-      'enc_l0_bwd': (np.asarray([[-0.09987763,-0.19987726],[-0.09983368,-0.19983764],[-0.09984706,-0.1998519,],[-0.09962509,-0.19965594]]),
-                     np.asarray([[-0.10002756],[-0.10004743],[-0.100041,],[-0.10013461]]),
-                     np.asarray([-3.16269543e-05,-6.89425797e-05,-6.18750637e-05,-8.94205179e-04])),
-      'enc_l1_fwd': (np.asarray([[-0.10003329,-0.20003363],[-0.10004379,-0.20003976],[-0.10003274,-0.20003073],[-0.09949255,-0.19953378]]),
-                     np.asarray([[-0.09999352],[-0.09999096],[-0.09999333],[-0.10010429]]),
-                     np.asarray([-7.18495066e-05,-9.96304589e-05,-7.29403910e-05,2.58315331e-03])),
-      'enc_l1_bwd': (np.asarray([[-0.10006139,-0.20005998],[-0.10006943,-0.2000725,],[-0.10005932,-0.20006149],[-0.099105,-0.19919929]]),
-                     np.asarray([[-0.09998865],[-0.09998529],[-0.09998781],[-0.10014705]]),
-                     np.asarray([-0.00012661,-0.00014598,-0.0001292,0.00507051])),
-      'enc_l2_fwd': (np.asarray([[-0.09999367,-0.19999345],[-0.09999148,-0.1999929,],[-0.09999393,-0.19999467],[-0.09944947,-0.19950114]]),
-                     np.asarray([[-0.10000134],[-0.10000204],[-0.10000141],[-0.10013182]]),
-                     np.asarray([-6.99323864e-05,-1.00616948e-04,-7.01395402e-05,-1.25058405e-02])),
-      'enc_l2_bwd': (np.asarray([[-0.09998975,-0.19999012],[-0.09998945,-0.19998839],[-0.09999086,-0.1999899,],[-0.09914374,-0.19928391]]),
-                     np.asarray([[-0.10000202],[-0.10000266],[-0.10000228],[-0.10015319]]),
-                     np.asarray([-0.00010539,-0.00011597,-0.00010555,-0.02152999])),
-      'decoder': (np.asarray([[-0.10115287,-0.19884713,-0.0999788,-0.19998185],[ 0.10264169,0.19735833,0.09995335,0.19996007],[-0.10086214,-0.19913787,-0.09998852,-0.19999012],[ 0.10191832,0.19808169,0.09997454,0.19997808],[-0.10116317,-0.19883683,-0.09997369,-0.19997761],[ 0.10266069,0.19733933,0.0999413,0.19995002],[-0.07204451,-0.22795549,-0.10052628,-0.20045052],[ 0.16965774,0.13034226,0.09874846,0.19892833]]),
-                  np.asarray([[-0.10005526,-0.19993505],[ 0.10012097,0.19985785],[-0.10003345,-0.19996063],[ 0.10007406,0.19991286],[-0.10008492,-0.19989935],[ 0.1001896,0.19977526],[-0.0986231,-0.20161855],[ 0.10325646,0.19617321]]),
-                  np.asarray([ 0.0016304,-0.00373591,0.00121925,-0.00271291,0.00164497,-0.00376278,-0.03953505,-0.09851094])),
-      'attender': (np.asarray([[-0.1,-0.2],[ 0.1,0.2]]), # TODO: why are these untrained?
-                    np.asarray([[-0.09999969,-0.19999962],[ 0.10000063,0.20000079]]),
-                    np.asarray([-1.65015184e-08,-3.30030367e-08]),
-                    np.asarray([[-0.0999989,-0.20000111]])),
-    }
-    self.assert_trained_seq2seq_params(expected, places=6, lr=10)
+  def test_mlp_att_grads_trained(self):
+    expected = (np.asarray([[ 6.15145327e-05,5.41474146e-05],[-1.41641664e-04,-1.24026701e-04]]),
+                np.asarray([[-9.45401825e-06,-1.82743112e-04],[ 1.28936317e-05,2.54007202e-04]]),
+                np.asarray([-0.00021929,0.0002702,]),
+                np.asarray([[ 0.00020377,-0.00013509]]))
+    self.assert_trained_mlp_att_grads(expected, places=7, epochs=3, adam=True, lr=100)
+
+  # def test_loss_three_epochs_adam(self):
+  #   self.assert_loss_value(8.912698745727539, places=2, epochs=3, lr=10, adam=True)
+  # def test_loss_ten_epochs_adam(self):
+  #   self.assert_loss_value(7.670589447021484, places=2, epochs=10, lr=10, adam=True)
+
+  # def test_all_params_one_epoch(self):
+  #   # useful regex: (?<!\[)\s+      ->      ,
+  #   expected = {
+  #     'trg_emb': np.asarray([[-0.08201125,0.11798876],[-0.2,0.2,],[-0.30986509,0.29013494],[-0.41663632,0.38336369],[-0.5,0.5,]]),
+  #     'transform': (np.asarray([[-0.11458226,-0.1818178,-0.29989332,-0.39990845],[ 0.11458226,0.1818178,0.29989332,0.39990845]]),
+  #                   np.asarray([ 0.01091676,-0.01091676])),
+  #     'out': (np.asarray([[-0.05089095,0.05089095],[-0.2470859,0.2470859,],[-0.32606718,0.32606718],[-0.42551777,0.42551777],[-0.45043823,0.45043823]]),
+  #             np.asarray([-5.37809563,3.61097455,3.60002279,3.58904791,-5.42194843])),
+  #     'enc_l0_fwd': (np.asarray([[-0.09993633,-0.19993669],[-0.09990665,-0.19991051],[-0.09992233,-0.19992436],[-0.09978127,-0.1998006,]]),
+  #                    np.asarray([[-0.10001528],[-0.10002846],[-0.1000223,],[-0.10008691]]),
+  #                    np.asarray([-1.90493865e-05,-5.03285955e-05,-3.61094171e-05,-4.47910599e-04])),
+  #     'enc_l0_bwd': (np.asarray([[-0.09987763,-0.19987726],[-0.09983368,-0.19983764],[-0.09984706,-0.1998519,],[-0.09962509,-0.19965594]]),
+  #                    np.asarray([[-0.10002756],[-0.10004743],[-0.100041,],[-0.10013461]]),
+  #                    np.asarray([-3.16269543e-05,-6.89425797e-05,-6.18750637e-05,-8.94205179e-04])),
+  #     'enc_l1_fwd': (np.asarray([[-0.10003329,-0.20003363],[-0.10004379,-0.20003976],[-0.10003274,-0.20003073],[-0.09949255,-0.19953378]]),
+  #                    np.asarray([[-0.09999352],[-0.09999096],[-0.09999333],[-0.10010429]]),
+  #                    np.asarray([-7.18495066e-05,-9.96304589e-05,-7.29403910e-05,2.58315331e-03])),
+  #     'enc_l1_bwd': (np.asarray([[-0.10006139,-0.20005998],[-0.10006943,-0.2000725,],[-0.10005932,-0.20006149],[-0.099105,-0.19919929]]),
+  #                    np.asarray([[-0.09998865],[-0.09998529],[-0.09998781],[-0.10014705]]),
+  #                    np.asarray([-0.00012661,-0.00014598,-0.0001292,0.00507051])),
+  #     'enc_l2_fwd': (np.asarray([[-0.09999367,-0.19999345],[-0.09999148,-0.1999929,],[-0.09999393,-0.19999467],[-0.09944947,-0.19950114]]),
+  #                    np.asarray([[-0.10000134],[-0.10000204],[-0.10000141],[-0.10013182]]),
+  #                    np.asarray([-6.99323864e-05,-1.00616948e-04,-7.01395402e-05,-1.25058405e-02])),
+  #     'enc_l2_bwd': (np.asarray([[-0.09998975,-0.19999012],[-0.09998945,-0.19998839],[-0.09999086,-0.1999899,],[-0.09914374,-0.19928391]]),
+  #                    np.asarray([[-0.10000202],[-0.10000266],[-0.10000228],[-0.10015319]]),
+  #                    np.asarray([-0.00010539,-0.00011597,-0.00010555,-0.02152999])),
+  #     'decoder': (np.asarray([[-0.10115287,-0.19884713,-0.0999788,-0.19998185],[ 0.10264169,0.19735833,0.09995335,0.19996007],[-0.10086214,-0.19913787,-0.09998852,-0.19999012],[ 0.10191832,0.19808169,0.09997454,0.19997808],[-0.10116317,-0.19883683,-0.09997369,-0.19997761],[ 0.10266069,0.19733933,0.0999413,0.19995002],[-0.07204451,-0.22795549,-0.10052628,-0.20045052],[ 0.16965774,0.13034226,0.09874846,0.19892833]]),
+  #                 np.asarray([[-0.10005526,-0.19993505],[ 0.10012097,0.19985785],[-0.10003345,-0.19996063],[ 0.10007406,0.19991286],[-0.10008492,-0.19989935],[ 0.1001896,0.19977526],[-0.0986231,-0.20161855],[ 0.10325646,0.19617321]]),
+  #                 np.asarray([ 0.0016304,-0.00373591,0.00121925,-0.00271291,0.00164497,-0.00376278,-0.03953505,-0.09851094])),
+  #     'attender': (np.asarray([[-0.1,-0.2],[ 0.1,0.2]]), # TODO: why are these untrained?
+  #                   np.asarray([[-0.09999969,-0.19999962],[ 0.10000063,0.20000079]]),
+  #                   np.asarray([-1.65015184e-08,-3.30030367e-08]),
+  #                   np.asarray([[-0.0999989,-0.20000111]])),
+  #   }
+  #   self.assert_trained_seq2seq_params(expected, places=6, lr=10)
+
+
+  # def test_all_params_one_epoch_adam(self):
+  #   # useful regex: (?<!\[)\s+      ->      ,
+  #   expected = {
+  #     'trg_emb': np.asarray([[-1.75759196e-05,1.99982435e-01],[-2.00000003e-01,2.00000003e-01],[-3.99967968e-01,2.00032040e-01],[-4.99980986e-01,3.00019026e-01],[-5.00000000e-01,5.00000000e-01]]),
+  #     'transform': (np.asarray([[-0.19997832,-0.1000174,-0.20287874,-0.3033388,],[ 0.19997832,0.1000174,0.20287874,0.3033388,]]),
+  #                   np.asarray([ 0.09997104,-0.09997104])),
+  #     'out': (np.asarray([[-6.44475222e-06,6.44475222e-06],[-2.99993277e-01,2.99993277e-01],[-3.99987876e-01,3.99987876e-01],[-4.99987602e-01,4.99987602e-01],[-4.00006384e-01,4.00006384e-01]]),
+  #             np.asarray([-0.09999994,0.09999993,0.09999992,0.0999999,-0.09999994])),
+  #     'enc_l0_fwd': (np.asarray([[-0.00473128,-0.1047574,],[-0.00327653,-0.10341273],[-0.00391212,-0.10401335],[-0.00142514,-0.10156112]]),
+  #                    np.asarray([[-0.1828548,],[-0.18999776],[-0.18757865],[-0.19648919]]),
+  #                    np.asarray([-0.08576292,-0.09408816,-0.09194765,-0.09929894])),
+  #     'enc_l0_bwd': (np.asarray([[-0.00251901,-0.10251159],[-0.00186585,-0.10191042],[-0.00202568,-0.10209057],[-0.00083642,-0.10091073]]),
+  #                    np.asarray([[-0.18970746],[-0.19374949],[-0.1928391,],[-0.1977047,]]),
+  #                    np.asarray([-0.09091013,-0.0956143,-0.09513772,-0.09964762])),
+  #     'enc_l1_fwd': (np.asarray([[-0.19132513,-0.29140514],[-0.19326439,-0.29263091],[-0.19119145,-0.29066783],[-0.00061932,-0.1006737,]]),
+  #                    np.asarray([[-0.03277878],[-0.02590235],[-0.03216105],[-0.19705713]]),
+  #                    np.asarray([-0.09578426,-0.09692363,-0.0958447,0.09987774])),
+  #     'enc_l1_bwd': (np.asarray([[-0.19510096,-0.2949917,],[-0.19564378,-0.29581988],[-0.19493818,-0.29510871],[-0.00035208,-0.10039338]]),
+  #                    np.asarray([[-0.02177877],[-0.01768639],[-0.02059451],[-0.19789474]]),
+  #                    np.asarray([-0.09756327,-0.09787969,-0.09761088,0.09993766])),
+  #     'enc_l2_fwd': (np.asarray([[-0.03331528,-0.13254735],[-0.02706053,-0.13079686],[-0.03424625,-0.13719964],[-0.00057114,-0.10062992]]),
+  #                    np.asarray([[-0.12979138],[-0.13919146],[-0.13082246],[-0.19765723]]),
+  #                    np.asarray([-0.09567371,-0.09695286,-0.09568593,-0.09997474])),
+  #     'enc_l2_bwd': (np.asarray([[-0.02356844,-0.12424017],[-0.02306087,-0.12141666],[-0.0256956,-0.12383554],[-0.00036795,-0.10043966]]),
+  #                    np.asarray([[-0.13893157],[-0.14568059],[-0.14186357],[-0.19797748]]),
+  #                    np.asarray([-0.09708674,-0.09734552,-0.09709109,-0.09998534])),
+  #     'decoder': (np.asarray([[-1.99726462e-01,-1.00273542e-01,-1.29794180e-02,-1.14841036e-01],[ 1.99880436e-01,1.00119568e-01,6.34786487e-03,1.07336700e-01],[-1.99634552e-01,-1.00365460e-01,-2.15895846e-02,-1.24240689e-01],[ 1.99835435e-01,1.00164570e-01,1.10447034e-02,1.12606354e-01],[-1.99728876e-01,-1.00271128e-01,-1.07286796e-02,-1.12374164e-01],[ 1.99881285e-01,1.00118719e-01,5.11157513e-03,1.05950944e-01],[-1.13099813e-05,-2.99988687e-01,-1.99402705e-01,-2.99302965e-01],[ 1.99995473e-01,1.00004531e-01,2.52030790e-04,1.00294210e-01]]),
+  #                 np.asarray([[-1.94587052e-01,-1.04642287e-01],[ 1.97452396e-01,1.02176145e-01],[-1.91362619e-01,-1.07435435e-01],[ 1.95904762e-01,1.03501700e-01],[-1.96409553e-01,-1.03045903e-01],[ 1.98359489e-01,1.01387605e-01],[-2.29150057e-04,-2.99805015e-01],[ 1.99902982e-01,1.00082561e-01]]),
+  #                 np.asarray([ 0.09980642,-0.09991542,0.09974132,-0.09988359,0.09980813,-0.09991604,-0.09999201,-0.09999679])),
+  #     'attender': (np.asarray([[-0.09996349,-0.20004468],[ 0.10007299,0.19991069]]),
+  #                   np.asarray([[-0.09092791,-0.18898019],[ 0.11663505,0.21985196]]),
+  #                   np.asarray([-0.00051911,-0.00103286]),
+  #                   np.asarray([[-0.07420623,-0.22579378]])),
+  #   }
+  #   self.assert_trained_seq2seq_params(expected, places=6, lr=10, adam=True)
 
 
   # def test_emb_weights_one_epoch(self):
